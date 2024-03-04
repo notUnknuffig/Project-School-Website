63479e60d8fccc2c99c09f38b461969516ecbd9d
notUnknuffig <139910467+notUnknuffig@users.noreply.github.com>
2024-03-04 15:24:45 +0100

    Folder Structure

--- menu.js
+++ menu.js
@@ -1,21 +1,26 @@
 window.addEventListener("scroll", scrollVar)
 window.addEventListener("resize", scrollVar)
+window.addEventListener("resize", menuResize)
 
 let menu_open = false
+const htmlDocument = document.documentElement
+const color_1_deg = 240;
+const color_2_deg = 200;
+const color_3_deg = 140;
 
 function scrollVar() {
-    const htmlDocument = document.documentElement
-    const scrollPercent = Math.min((htmlDocument.scrollTop / htmlDocument.clientHeight) * 100, 200)
-    htmlDocument.style.setProperty("--scroll", scrollPercent)
+    const scrollPercent = Math.min((htmlDocument.scrollTop / htmlDocument.clientHeight) * 100, 200);
+    htmlDocument.style.setProperty("--scroll", scrollPercent);
 
-    const colorValue_section_1 = (197 - 240) * (Math.min(Math.max(scrollPercent -60, 0), 40)/ 40)
-    const colorValue_section_2 = (140 - 197) * (Math.min(Math.max(scrollPercent -160, 0), 40)/ 40)
+    const colorValue_section_1 = (color_2_deg - color_1_deg) * (Math.min(Math.max(scrollPercent -60, 0), 40)/ 40);
+    const colorValue_section_2 = (color_3_deg - color_2_deg) * (Math.min(Math.max(scrollPercent -160, 0), 40)/ 40);
 
-    
-    const color = 240 + colorValue_section_1 + colorValue_section_2
-    
-    htmlDocument.style.setProperty("--value-clr", color)
+    const color = 240 + colorValue_section_1 + colorValue_section_2;
 
+    htmlDocument.style.setProperty("--value-clr", color);
+}
+
+function menuResize() {
     var menuButton = document.getElementById("menu-button");
     var menu = document.getElementsByClassName("menu-item-container");
 
@@ -25,7 +30,7 @@ function scrollVar() {
             menu_open = false;
             menu[0].style.visibility = "hidden"
             menu[0].style.transform = "translateY(0)"
-            menuButton.style.backgroundImage = "url('img/menu-icon.svg')";
+            menuButton.style.backgroundImage = "url('../img/menu-icon.svg')";
         }
     }
     else {
@@ -33,7 +38,7 @@ function scrollVar() {
         menu[0].style.visibility = "visible"
         menu[0].style.animation = "none"
         menu[0].style.transform = "translateY(var(--menu-scroll, 0))";
-        menuButton.style.backgroundImage = "url('img/exit-icon.svg')";
+        menuButton.style.backgroundImage = "url('../img/exit-icon.svg')";
     }
 }
 
@@ -53,7 +58,7 @@ function revealMenu() {
             menu[0].style.pointerEvents = "all"
         })
         menu[0].style.transform = "translateY(0)"
-        menuButton.style.backgroundImage = "url('img/exit-icon.svg')";
+        menuButton.style.backgroundImage = "url('../img/exit-icon.svg')";
     } else {
         menu_open = false;
         menu[0].style.animation = "open-close-menubar 0.5s ease-in-out reverse backwards"
@@ -63,6 +68,6 @@ function revealMenu() {
             menu[0].style.pointerEvents = "none"
         })
         menu[0].style.transform = "translateY(calc(1% * min((max( var(--scroll), 30 ) * 3 - 190), 0)))";
-        menuButton.style.backgroundImage = "url('img/menu-icon.svg')";
+        menuButton.style.backgroundImage = "url('../img/menu-icon.svg')";
     }
 }
